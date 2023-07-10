bba264ed05894ba1ea316e698cf0549f85e53f78
Kasper Masznicz <kmaszn19@student.aau.dk>
2023-07-10 14:33:56 +0200

    part 3

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,4 @@
 using Test
-include("../src/preprocessing.jl")
-
-@test countSpecialCharacters("This! No? Dont @ me then") == 3
-@test nGram(cleanTokenizer("Hello this is a test for nGram"), 2) == [["hello", "this"], ["this", "is"], ["is", "a"], ["a", "test"], ["test", "for"], ["for", "ngram"]]
-@test capsRatio("Don't WATCH") == 0.5
-@test wordcountScore("This is it or maybe it's not...", Dict(["it" => 1])) == 2
-@test removeSpecialCharacters("Well this is f**king bullshit!") == "Well this is fking bullshit"
-@test quickStemmer("lovely home") == "love home"
-@test cleanTokenizer("Hello there!") == ["hello", "there"]
 
 #Doesn't work without import
 #= @testitem "countSpecialCharacters test" begin
