5c69617ddd7cf5078a68c1b41c7f1a55961b4e56
Kasper Masznicz <kmaszn19@student.aau.dk>
2023-07-15 21:23:00 +0200

    Removing tests that requires a path

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,17 +6,11 @@ end
 @testset "normalize" begin
     include("test_normalize.jl")
 end
-@testset "train" begin
-    include("test_train.jl")
-end
 
 #Nice to have for vscode testing.
-#= @testitem "preprocessing" begin
+@testitem "preprocessing" begin
     include("test_preprocess.jl")
 end
 @testitem "normalizer" begin
     include("test_normalize.jl")
 end
-@testitem "train" begin
-    include("test_train.jl")
-end =#
--- test/test_train.jl
+++
@@ -1,4 +0,0 @@
-using Test, YTclickbaitClassifier
-vector = [0.1, 0.2, 0.3, 0.4, 0.5]
-@test typeof(loadModel(reshape(vector, size(vector, 1), 1))[1]) == Float32
-@test loadModel(reshape(vector, size(vector, 1), 1))[1] == true
